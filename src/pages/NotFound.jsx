4a8d282b0838cc3f71e8f95267c16dfa61bfb1f2
Manish kumar <manishnkb7@gmail.com>
2026-02-23 03:56:16 +0530

    adding NotFound component

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+import { useLocation, Link } from "react-router-dom";
+
+const NotFound = () => {
+  const location = useLocation();
+
+  return (
+    <div className="notfound">
+      <h1>404 - Page Not Found</h1>
+      <p>Invalid URL: {location.pathname}</p>
+      <Link to="/">Go Home</Link>
+    </div>
+  );
+};
+
+export default NotFound;
